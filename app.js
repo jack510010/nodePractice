2703e6fc19c81ab0ea2303afaa643db64b22d5f2
Albert <jack25512909@gmail.com>
2022-02-02 18:22:23 +0800

    res.send() 可以送的東西有很多
    res.send(new Buffer('wahoo'));
    res.send({ some: 'json' });
    res.send('<p>some html</p>');
    res.status(404).send('Sorry, cant find that');

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {   // '/' 就是homepage的意思
-    res.send('You are on the homepage.')
+
+    //res.send('<h1>You are on the homepage.</h1>')   // res.send() 可以送html的標籤
+
+    //res.sendFile(__dirname + '/index.html');  // res.sendFile() 可以送整個html檔案，記得要加__dirname
+
+    let jackson = {
+        name: 'Jackson',
+        age: 22,
+    };
+    res.send(jackson);  // json格式
 });
 
 app.get('/albert', (req, res) => {
