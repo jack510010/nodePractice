4b9321df9c9e2cef09e465ec36b0485cce03a402
Albert <jack25512909@gmail.com>
2022-02-02 14:22:22 +0800

    簡易版 node server

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
-//fs => file system 可以在javascript裡面製作一些文件
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
 
-const fs = require("fs");
+const server = http.createServer((req, res) => {
 
-fs.readFile("./try.txt", "utf8", (e, data) => {
-    if(e) throw e;
-    console.log(data);
+    if (req.url == '/') {
+        fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            }); // 寫檔頭，『{'Content-Type': 'text/html'}』，語法容易忘記要記一下。text/html可以換成別的方式
+            res.write(data);
+
+            res.end();
+        })
+
+    } else {
+        let parsedUrl = url.parse(req.url);
+
+        res.write('Hello, ' + parsedUrl.pathname);
+        res.end();
+    }
+
 });
+
+server.listen(3501, () => {
+    console.log('server is running on 3501.');
+}); // listen()監聽的是port number
