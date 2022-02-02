579a5f2b20ff22cebf447d32464a7370ef28d213
Albert <jack25512909@gmail.com>
2022-02-02 17:19:04 +0800

    不同的路由route可以去到不同的頁面。

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => {   // '/' 就是homepage的意思
     res.send('You are on the homepage.')
+});
+
+app.get('/albert', (req, res) => {
+    res.send("This is Albert's page.");
+});
+
+app.get('/mike', (req, res) => {
+    res.send("This is Mike's page.");
 });
 
 let port = 3000
