a40bb0a0704c547d23466c40a3c0c03288e89b5b
Albert <jack25512909@gmail.com>
2022-02-02 14:41:29 +0800

    玩玩看cowsay  娛樂性質

--- app.js
+++ app.js
@@ -1,29 +1,9 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
-const path = require('path');
+const cowsay = require("cowsay");
 
-const server = http.createServer((req, res) => {
+console.log(cowsay.say({
+    text : "I'm a Taiwanese cow",
+    e : "口口",
+    T : "U "
+}));
 
-    if (req.url == '/') {
-        fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
-            res.writeHead(200, {
-                'Content-Type': 'text/html'
-            }); // 寫檔頭，『{'Content-Type': 'text/html'}』，語法容易忘記要記一下。text/html可以換成別的方式
-            res.write(data);
-
-            res.end();
-        })
-
-    } else {
-        let parsedUrl = url.parse(req.url);
-
-        res.write('Hello, ' + parsedUrl.pathname);
-        res.end();
-    }
-
-});
-
-server.listen(3501, () => {
-    console.log('server is running on 3501.');
-}); // listen()監聽的是port number
+// or cowsay.think()
